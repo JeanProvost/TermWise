b0d55c3879071fc83da2bc960c9140aa63fc1010
JeanProvost <jprov310@gmail.com>
2025-07-14 21:10:17 -0600

    FIxing border

--- src/components/StateView.tsx
+++ src/components/StateView.tsx
@@ -5,9 +5,11 @@ interface StateViewProps {
 }
 
 const StateView: React.FC<StateViewProps> = ({ children }) => (
-  <div className="flex flex-col items-center justify-center py-12">
-    {children}
+  <div className="rounded-lg bg-primary p-[5px]">
+    <div className="flex flex-col items-center justify-center rounded-lg border border-primary py-12">
+      {children}
+    </div>
   </div>
 );
 
-export default StateView; 
+export default StateView;
